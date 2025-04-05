f1c29df476b68945ec0d3a47495c30a00fb8f1c1
ayushmishradev <ayushmishrawebdev@gmail.com>
2025-04-05 14:39:04 +0530

    working on linked list and doubly linked list

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,84 +1,50 @@
 class Node {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-      this.prev = null;
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
   }
-  
-  class  DoublyLinkedList {
-      constructor(value) {
-          this.head = {
-            value : value,
-            next: null,
-            prev: null
-          } 
-          this.tail = this.head;
-          this.length = 1; 
-      }
-  
-      append(value) {
-        const newNode = new Node(value);
-        newNode.prev = this.tail;
-        this.tail.next = newNode;
-        this.tail = newNode;
-        this.length++
-        return this
-      }
-  
-      prepend(value){
-        const newNode = new Node(value);
-        newNode.next = this.head;
-        this.head = next;
-        this.length++;
-        return this;
-      }
-  
-      printList() {
-        const array = [];
-        let currentNode = this.head;
-        while(currentNode !== null) {
-          array.push(currentNode.value);
-          currentNode = currentNode.next;
-        }
-        return array;
-      }
-  
-      insert(index, value) {
-        //check the paramas
-        if(index >= this.length) {
-          return this.append(value);
-        }
-        const newNode = {
-          value : value,
-          next: null
-        }
-        const leader = this.traverseToLeader(index-1);
-        const holdingPointer = leader.next;
-        leader.next = newNode;
-        newNode.next = holdingPointer;
-        this.length++
-        return this.printList();
-      }
-  
-      remove(index) {
-        const leader = this.traverseToLeader(index-1);
-        const unwantedNode = leader.next;
-        leader.next = unwantedNode.next;
-        this.length--;
-      }
-  
-      traverseToLeader(index) {
-        let counter = 0;
-        let currentNode = this.head;
-        while(counter !== index) {
-          currentNode = currentNode.next;
-          counter++
-        }
-        return currentNode;
-      }
+
+  pop() {
+    if (!this.head) {
+      return undefined
+    }
+    if (this.length === 1) {
+      this.head = 0;
+      this.tail = 0;
+    } else {
+      let temp = this.tail;
+      this.tail = temp.prev;
+      this.tail.next = null;
+      temp.prev = null
+      this.length--;
+      return temp;
+    }
+
   }
-  
-  const myLinkedList = new LinkedList(10);
-  
-  console.log(myLinkedList);
+}
--- linkedList.js
+++ linkedList.js
@@ -95,4 +95,85 @@ class LinkedList {
     return false;
   }
 
+  insert(index, value) {
+    if(index < 0 || index > this.length) {
+      return false;
+    }
+    if(index=== 0) {
+      return this.unShift(value);
+    }
+    if(index=== this.length) {
+      return this.push(value);
+    }
+    const node = newNode(value);
+    const temp = this.get(index-1);
+    newNode.next = temp.next;
+    temp.next = newNode;
+    this.length++
+    return true;
+  }
+
+  remove(index) {
+    if(index < 0 || index > this.length){
+      return "Please enter correct index"
+    }
+    if(index===0){
+      return this.shift();
+    }
+    if(index===this.length-1){
+      return this.pop();
+    }
+    const before = this.get(index-1);
+    const temp = before.next;
+
+    before.next = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
+
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let prev = null;
+    let next = temp.next;
+    for(let i =0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev
+      prev = temp;
+      temp = next;
+    }
+    return this;
+  }
+
+  findMiddleOfLinkedList() {
+    if( ! this.head) {
+      return "List is empty"
+    }
+    let slow = this.head;
+    let fast = this.head;
+    while(slow.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  }
+
+  findLoop() {
+    if( ! this.head) {
+      return null;
+    }
+    const array = [];
+    array.push(this.head.value);
+    let temp = this.head;
+    for(let i =0; i< this.length; i++) {
+      if(array.indexOf(temp.next) > -1) {
+        return true;
+      }
+      array.push(temp.next);
+    }
+    return false;
+  }
+
 }
