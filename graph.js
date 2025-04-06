a53220980d7f5993b5a79d04971f53b4df9d218a
ayushmishradev <ayushmishrawebdev@gmail.com>
2025-04-06 21:35:10 +0530

    graph updated

--- graph.js
+++ graph.js
@@ -1,15 +1,44 @@
 class Graph {
     constructor() {
-        this.numberOfNodes = 0;
         this.adjacentList = {}
     }
-    addVertex(node) {
-        this.adjacentList[node] = {};
-        this.numberOfNodes++;
-    }
-    addEdge(node1, node2) {
-      this.adjacentList[node1].push(node2);
-      this.adjacentList[node2].push(node1);
 
+    addVertex(vertex) {
+        if (!this.adjacentList[vertex]) {
+            this.adjacentList[vertex] = []
+            return true;
+        }
+        return false
+    }
+
+    addEdge(vertex1, vertex2) {
+        if (this.adjacentList[vertex1] && this.adjacentList[vertex2]) {
+            this.adjacentList[vertex1].push(vertex2);
+            this.adjacentList[vertex2].push(vertex1);
+            return true;
+        }
+        return false;
+    }
+
+    removeEdge(vertex1, vertex2) {
+        if (this.adjacentList[vertex1] && this.adjacentList[vertex2]) {
+            this.adjacentList[vertex1] = this.adjacentList[vertex1]
+                .filter((vertx) => vertx !== vertex2)
+            this.adjacentList[vertex2] = this.adjacentList[vertex2]
+                .filter((vertx) => vertx !== vertex1)
+            return true;
+        }
+        return false;
+    }
+
+    removeVertex(vertex) {
+        if (!this.adjacentList[vertex]) {
+            return undefined
+        }
+        while (this.adjacentList[vertex].length) {
+            let temp = this.adjacentList[vertex].pop();
+            this.removeEdge(vertex, temp);
+        }
+        delete this.adjacentList[vertex];
     }
 }
