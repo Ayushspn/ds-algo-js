8934a5dbdb81babf1a7c302f6361ebcfc423f81b
ayushmishradev <ayushmishrawebdev@gmail.com>
2025-04-10 00:33:44 +0530

    binary tree

--- binaryTreeUsingRecusrion.js
+++ binaryTreeUsingRecusrion.js
@@ -10,3 +10,16 @@ function rContaints(value, currentNode = this.root) {
         return this.rContaints(value, currentNode.right);
     }
 }
+
+function deletNodes(value, currentNoderoot) {
+    if(currentNode === null) {
+        return null;
+    }
+
+    if(value < currentNode.Value) {
+        currentNode.left = this.deletNodes(value, currentNode.left)
+    } if(value > currentNode.Value) {
+        currentNode.right = this.deletNodes(value, currentNode.right)
+    }
+    return currentNode;
+}
