4636b014427215f4c5d4bf25f9431737006c3248
ayushmishradev <ayushmishrawebdev@gmail.com>
2025-07-13 13:45:08 +0530

    second solution

--- containCommonItem.js
+++ containCommonItem.js
@@ -7,3 +7,21 @@ function containCommonItems(arr1, arr2) {
     }
     return false;
 }
+
+
+function containCommonItems2(arr1, arr2) {
+   /**
+    * Loop through the first array and convert it to a object
+    * Then loop through the second array and check if the object has the item
+    */
+    const obj = {};
+    for (let item of arr1) {
+        obj[item] = true;
+    }
+    for (let item of arr2) {
+        if (obj[item]) {
+            return true;
+        }
+    }   
+    return false;
+}
